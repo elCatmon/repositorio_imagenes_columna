10cbd0e2e637300f4245cfb3ff632b907470fbbc
Cesar Ortega <89800127+elCatmon@users.noreply.github.com>
2024-10-09 20:33:17 -0600

    update

--- pruebas/src/components/Miniaturas/ThumbnailGallery.js
+++ pruebas/src/components/Miniaturas/ThumbnailGallery.js
@@ -6,21 +6,50 @@ const ThumbnailGallery = ({ onThumbnailClick }) => {
   const [images, setImages] = useState([]);
   const [loaded, setLoaded] = useState(false);
   const [error, setError] = useState(null);
-  const [page, setPage] = useState(1); // Página actual
-  const [hasMore, setHasMore] = useState(true); // Si hay más imágenes para cargar
-
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  
   const [tipoEstudio, setTipoEstudio] = useState('');
   const [region, setRegion] = useState('');
+  const [subregion, setSubregion] = useState('');
+
+  // Define subregiones según la región seleccionada
+  const subregionesOptions = {
+    '02': [
+      { value: '03', label: 'Cervical' },
+      { value: '04', label: 'Torácica' },
+      { value: '05', label: 'Lumbar' },
+      { value: '06', label: 'Sacra' },
+      { value: '07', label: 'Coxis' }
+    ],
+    '09': [
+      { value: '10', label: 'Hombro' },
+      { value: '11', label: 'Humero' },
+      { value: '12', label: 'Codo' },
+      { value: '13', label: 'Antebrazo' },
+      { value: '14', label: 'Muñeca' },
+      { value: '15', label: 'Mano' }
+    ],
+    '17': [
+      { value: '18', label: 'Femur' },
+      { value: '19', label: 'Rodilla' },
+      { value: '20', label: 'Tibia y Perone' },
+      { value: '21', label: 'Tobillo' },
+      { value: '22', label: 'Pie' }
+    ],
+  };
 
   const fetchImages = useCallback(async (pageNumber) => {
     if (!tipoEstudio) return;
+    
+    const selectedRegion = subregion || region; // Utilizar subregion si está seleccionada
 
     try {
       const query = new URLSearchParams({
         tipoEstudio,
-        region,
+        region: selectedRegion,
         page: pageNumber,
-        limit: 18 // Limitar a 18 imágenes por página
+        limit: 18
       }).toString();
 
       const response = await fetch(`${BASE_URL}/thumbnails?${query}`);
@@ -28,9 +57,9 @@ const ThumbnailGallery = ({ onThumbnailClick }) => {
       if (response.ok) {
         const data = await response.json();
         if (data.length < 18) {
-          setHasMore(false); // No hay más imágenes para cargar
+          setHasMore(false);
         }
-        setImages(prevImages => [...prevImages, ...data]); // Añadir nuevas imágenes al estado
+        setImages(prevImages => [...prevImages, ...data]);
         setLoaded(true);
       } else {
         setError('Error al obtener las imágenes');
@@ -38,21 +67,32 @@ const ThumbnailGallery = ({ onThumbnailClick }) => {
     } catch (error) {
       setError('Error en la solicitud de imágenes: ' + error.message);
     }
-  }, [tipoEstudio, region]);
+  }, [tipoEstudio, region, subregion]);
 
   useEffect(() => {
-    setImages([]); // Limpiar imágenes cuando cambian los filtros
-    setPage(1); // Resetear la página
-    setHasMore(true); // Volver a habilitar el botón de cargar más
-    fetchImages(1); // Cargar la primera página
-  }, [fetchImages, tipoEstudio, region]);
+    // Resetear estado de imágenes y paginación al cambiar filtros
+    setImages([]);
+    setPage(1);
+    setHasMore(true);
+    setLoaded(false);
+    fetchImages(1);
+  }, [fetchImages, tipoEstudio, region, subregion]);
 
   const handleLoadMore = () => {
     if (hasMore) {
       const nextPage = page + 1;
       setPage(nextPage);
-      fetchImages(nextPage); // Cargar la siguiente página
+      fetchImages(nextPage);
     }
+  };
+
+  const handleRegionChange = (e) => {
+    setRegion(e.target.value);
+    setSubregion(''); // Resetear subregión al cambiar la región
+  };
+
+  const handleSubregionChange = (e) => {
+    setSubregion(e.target.value);
   };
 
   if (error) {
@@ -60,45 +100,57 @@ const ThumbnailGallery = ({ onThumbnailClick }) => {
   }
 
   return (
-    <div className="thumbnail-gallery-container" style={{ marginLeft:'100px'}}>
-      <h1 className="text-5xl font-extrabold mb-4 animate-reveal" style={{color: '#666666', backgroundColor: 'transparent !important', marginTop: '20px', fontWeight: '900', fontFamily:'Poppins' }}>
-   Estudios médicos
-</h1>
+    <div className="thumbnail-gallery-container" style={{ marginLeft: '100px' }}>
+      <h1 className="text-5xl font-extrabold mb-4 animate-reveal" style={{ color: '#666666', marginTop: '20px', fontWeight: '900', fontFamily: 'Poppins' }}>
+        Estudios médicos
+      </h1>
 
       <div className="filters" style={filterStyle}>
         <label>
           Tipo de Estudio:
           <select value={tipoEstudio} onChange={(e) => setTipoEstudio(e.target.value)}>
-          <option value="">Seleccione</option>
-              <option value="01">Radiografía</option>
-              <option value="02">Tomografía Computarizada</option>
-              <option value="03">Resonancia Magnética</option>
-              <option value="04">Ultrasonido</option>
-              <option value="05">Mamografía</option>
-              <option value="06">Angiografía</option>
-              <option value="07">Medicina Nuclear</option>
-              <option value="08">Radio Terapia</option>
-              <option value="09">Fluoroscopia</option>
+            <option value="">Seleccione</option>
+            <option value="01">Radiografía</option>
+            <option value="02">Tomografía Computarizada</option>
+            <option value="03">Resonancia Magnética</option>
+            <option value="04">Ultrasonido</option>
+            <option value="05">Mamografía</option>
+            <option value="06">Angiografía</option>
+            <option value="07">Medicina Nuclear</option>
+            <option value="08">Radio Terapia</option>
+            <option value="09">Fluoroscopia</option>
           </select>
         </label>
-            <label>
-              Región:
-              <select value={region} onChange={(e) => setRegion(e.target.value)}>
+        
+        <label>
+          Región:
+          <select value={region} onChange={handleRegionChange}>
+            <option value="">Seleccione</option>
+            <option value="00">Desconocido</option>
+            <option value="01">Craneo</option>
+            <option value="02">Columna Vertebral</option>
+            <option value="08">Torax</option>
+            <option value="09">Extremidad Superior</option>
+            <option value="16">Pelvis</option>
+            <option value="17">Extremidad Inferior</option>
+          </select>
+        </label>
+
+        {region && subregionesOptions[region] && (
+          <label>
+            Subregión:
+            <select value={subregion} onChange={handleSubregionChange}>
               <option value="">Seleccione</option>
-              <option value="01">Cabeza</option>
-              <option value="02">Cuello</option>
-              <option value="03">Torax</option>
-              <option value="04">Abdomen</option>
-              <option value="05">Pelvis</option>
-              <option value="06">Brazo</option>
-              <option value="07">Manos</option>
-              <option value="08">Pernas</option>
-              <option value="09">Rdilla</option>
-              <option value="10">Tobillo</option>
-              <option value="11">Pie</option>
-              </select>
-            </label>
+              {subregionesOptions[region].map(option2 => (
+                <option key={option2.value} value={option2.value}>
+                  {option2.label}
+                </option>
+              ))}
+            </select>
+          </label>
+        )}
       </div>
+
       <div className="thumbnail-gallery">
         {loaded ? (
           images.length > 0 ? (
@@ -113,7 +165,7 @@ const ThumbnailGallery = ({ onThumbnailClick }) => {
                     src={image}
                     alt={`Thumbnail ${index}`}
                     style={thumbnailStyle}
-                    onError={(e) => { e.currentTarget.src = 'https://via.placeholder.com/150'; }} // Imagen de sustitución en caso de error
+                    onError={(e) => { e.currentTarget.src = 'https://via.placeholder.com/150'; }} 
                   />
                 </div>
               ))}
