f81fe954a249dd4dcaa1eb362459576d5d099b47
Cesar Ortega <89800127+elCatmon@users.noreply.github.com>
2024-11-20 12:31:04 -0600

    Update ConsultarDonaciones.js

--- pruebas/src/components/Donaciones/ConsultarDonaciones.js
+++ pruebas/src/components/Donaciones/ConsultarDonaciones.js
@@ -43,11 +43,6 @@ function Estudios() {
     setLoading(true);
     setError(null);
 
-    if (!isValidCURP(filtros.curp)) {
-      alert('Por favor, ingresa un CURP válido.');
-      return;
-    }
-
     const queryParams = new URLSearchParams();
     if (filtros.folio) queryParams.append("folio", filtros.folio);
     if (filtros.correo) queryParams.append("correo", filtros.correo);
@@ -68,11 +63,6 @@ function Estudios() {
     } finally {
       setLoading(false);
     }
-  };
-
-  const isValidCURP = (curp) => {
-    const curpRegex = /^[A-Z]{4}\d{6}[HM][A-Z]{5}[A-Z0-9]{2}$/;
-    return curpRegex.test(curp);
   };
 
   return (
